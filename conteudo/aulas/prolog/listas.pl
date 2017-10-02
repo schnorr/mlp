b7b1fa6b0676577aaf2d4489bb1336958a46aefc
Lucas M. Schnorr <schnorr@inf.ufrgs.br>
2017-10-01 23:34:44 -0300

    implementation of soma

--- conteudo/aulas/prolog/listas.pl
+++ conteudo/aulas/prolog/listas.pl
@@ -3,3 +3,7 @@ concat([X|L1], L2, [X|D]) :- concat(L1, L2, D).
 
 membro(Elemento, [Elemento|_]) :- !.
 membro(Elemento, [_|Cauda]) :- membro(Elemento, Cauda).
+
+soma([], 0).
+soma([C|R], SOMA) :- soma(R, RESTO), SOMA is C + RESTO.
+
