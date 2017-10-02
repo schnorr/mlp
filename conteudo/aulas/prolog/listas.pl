6353ec78fbc50c3c3ad74528684a505bc50de2e1
Lucas M. Schnorr <schnorr@inf.ufrgs.br>
2017-10-01 23:34:31 -0300

    better implementation of member with cut

--- conteudo/aulas/prolog/listas.pl
+++ conteudo/aulas/prolog/listas.pl
@@ -1,5 +1,5 @@
 concat([], L2, L2) :- !.
 concat([X|L1], L2, [X|D]) :- concat(L1, L2, D).
 
-membro(Elemento, [Elemento|_]).
+membro(Elemento, [Elemento|_]) :- !.
 membro(Elemento, [_|Cauda]) :- membro(Elemento, Cauda).
