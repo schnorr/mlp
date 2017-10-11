4ccb4f632203327a6e36a5787b4214587576c32e
Lucas M. Schnorr <schnorr@inf.ufrgs.br>
2017-10-10 22:59:07 -0300

    make this example compiles with union

--- conteudo/aulas/tipos/struct.c
+++ conteudo/aulas/tipos/struct.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<limits.h>
 
 typedef char _Booleano;
 
@@ -14,10 +15,22 @@ struct element2 {
   _Booleano metallic;
   int atomic_number;
   double atomic_weight;
+}; 
+
+union xis {
+  int i;
+  long l;
 };
 
 int main (int argc, char **argv)
 {
 	printf ("struct element = %d\n", sizeof(struct element));
 	printf ("struct element2 = %d\n", sizeof(struct element2));
+
+	union xis K;
+	K.i = INT_MAX;
+	K.i++;
+	K.i++;
+	K.i++;
+	printf("%ld\n", K.i);
 }
