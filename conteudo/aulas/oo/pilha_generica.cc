73eff8d6def7c4487dabf9c683522ab6d6420748
Lucas M. Schnorr <schnorr@inf.ufrgs.br>
2017-11-29 15:17:14 -0200

    exemplos

---
+++ conteudo/aulas/oo/pilha_generica.cc
@@ -0,0 +1,72 @@
+#include <iostream>
+
+template <class TipoGenerico>
+class Pilha {
+private:
+  TipoGenerico *stackPtr;
+  int maxLen;
+  int topSub;
+
+public:
+  Pilha() {
+    stackPtr = new TipoGenerico [100];
+    maxLen = 99;
+    topSub = -1;
+  };
+
+  Pilha(int size) {
+    stackPtr = new TipoGenerico [size];
+    maxLen = size - 1;
+    topSub = -1;
+  };
+
+
+  ~Pilha(){
+    delete stackPtr;
+  };
+
+  void empilha(TipoGenerico number) {
+    if (topSub == maxLen){
+      std::cerr << "Error in push - stack is full" << std::endl;
+    }else{
+      stackPtr[++topSub] = number;
+    }
+  };
+
+  void desempilha() {
+    if (vazio()){
+      std::cerr << "Error in pop - stack is empty" << std::endl;
+    }else{
+      topSub--;
+    }
+  };
+
+  int topo() {
+    if (vazio()){
+      std::cerr << "Error in top - stack is empty" << std::endl;
+    }else{
+      return stackPtr[topSub];
+    }
+  };
+
+  int vazio() {
+    return topSub == -1;
+  };
+};
+
+int main()
+{
+  Pilha<int> pInteiro(100);
+  pInteiro.empilha(1);
+  pInteiro.desempilha();
+
+  // Pilha<float> pFlutuante(100);
+  // pFlutuante.empilha(2.3);
+  // pFlutuante.desempilha();
+
+  // Pilha<Pilha<int>* > p;
+  // p.empilha(&pInteiro);
+  // p.empilha(&pInteiro);
+  // p.desempilha();
+  // p.desempilha();
+}
---
+++ conteudo/aulas/oo/pilha_generica.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Pilha <TipoGenerico> {
+  
+  private ArrayList<TipoGenerico> stackRef;
+  private int maxLen;
+
+  public Pilha () {
+    stackRef = new ArrayList<TipoGenerico> ();
+    maxLen = 99;
+  }
+
+  public void empilha (TipoGenerico valor) {
+    if (stackRef.size() == maxLen){
+      System.out.println ("Error in push - stack is full");
+    }else{
+      stackRef.add (valor);
+    }
+  }
+
+  //...
+}
