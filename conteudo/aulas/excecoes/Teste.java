7b90610120878651bcf038185e897cf8ee8acd1b
Lucas M. Schnorr <schnorr@inf.ufrgs.br>
2017-12-10 22:56:02 -0200

    put finally

--- conteudo/aulas/excecoes/Teste.java
+++ conteudo/aulas/excecoes/Teste.java
@@ -1,12 +1,13 @@
 class Teste {
-    public static void main (String[] a)  {
-	try {
-	  int numerador = 10;
-	  int denominador = 0;
-	  if (denominador == 0) throw new Exception();
-	  else System.out.println (numerador/denominador);
-        } catch (Exception e){
-	  System.out.println ("deu erro");  
-        }
+  public static void main (String[] a)  {
+    try {
+      int numerador = 10;
+      int denominador = 1;
+      System.out.println (numerador/denominador);
+    } catch (Exception e){
+      System.out.println ("deu erro");
+    }finally{
+      System.out.println("Sempre executa");
     }
+  }
 }
