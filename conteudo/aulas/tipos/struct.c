1bc2717eb6f5e0e4a7ef3b1f391b1327bd6f9460
Lucas M. Schnorr <schnorr@inf.ufrgs.br>
2018-09-17 21:30:03 -0300

    a third example

--- conteudo/aulas/tipos/struct.c
+++ conteudo/aulas/tipos/struct.c
@@ -13,19 +13,34 @@ struct element {
 struct element2 {
   char name[2];
   _Booleano metallic;
+  char c;
   int atomic_number;
   double atomic_weight;
 }; 
+
+struct element3 {
+  char name[30];
+  struct {
+    char name[2];
+    _Booleano metallic;
+    int atomic_number;
+    double atomic_weight;
+  } element_yielded;
+};
+
 
 union xis {
   int i;
   long l;
 };
 
+
+
 int main (int argc, char **argv)
 {
 	printf ("struct element = %d\n", sizeof(struct element));
 	printf ("struct element2 = %d\n", sizeof(struct element2));
+	printf ("struct element3 = %d\n", sizeof(struct element3));
 
 	union xis K;
 	K.i = INT_MAX;
