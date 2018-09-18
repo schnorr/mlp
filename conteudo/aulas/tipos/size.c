3da9f1469a2238baf5729d63de874f6efe34db33
Lucas M. Schnorr <schnorr@inf.ufrgs.br>
2018-09-17 21:30:11 -0300

    this shall not compile

--- conteudo/aulas/tipos/size.c
+++ conteudo/aulas/tipos/size.c
@@ -5,6 +5,7 @@ int main (int argc, char **argv)
    printf ("int = %d\n", sizeof(int));
    printf ("long int = %d\n", sizeof(long int));
    printf ("long long int = %d\n", sizeof(long long int));
+   printf ("long long float = %d\n", sizeof(long long float);)
    printf ("unsigned long long int = %d\n", sizeof(unsigned long long int));
    printf ("float = %d\n", sizeof(float));
    printf ("double = %d\n", sizeof(double));
